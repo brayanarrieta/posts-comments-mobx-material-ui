383c865e6c41e6783d4379603547ccefa833a531
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2020-03-27 14:08:15 -0600

    Integrate eslint and refactor

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { CssBaseline, Container } from '@material-ui/core';
 import Header from './components/Header';
 
 function App() {
-	return (
-		<React.Fragment>
-			<CssBaseline />
-			<Container>
-				<Header />
-			</Container>
-		</React.Fragment>
-	);
+  return (
+    <>
+      <CssBaseline />
+      <Container>
+        <Header />
+      </Container>
+    </>
+  );
 }
 
 export default App;
