9a2d8f8dadb53e4d31e6fba0952d2dd6e16431cf
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2020-03-29 11:00:10 -0600

    Fix problem in CategoriesTable component

--- src/components/CategoriesTable.js
+++ src/components/CategoriesTable.js
@@ -3,61 +3,45 @@ import MaterialTable from 'material-table';
 
 const columns = [
   { title: 'Name', field: 'name' },
-  { title: 'Surname', field: 'surname' },
-  { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-  {
-    title: 'Birth Place',
-    field: 'birthCity',
-    lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-  },
-];
-
-const defaultData = [
-  {
-    name: 'Mehmet',
-    surname: 'Baran',
-    birthYear: 1987,
-    birthCity: 63,
-  },
-  {
-    name: 'Zerya Betül',
-    surname: 'Baran',
-    birthYear: 2017,
-    birthCity: 34,
-  },
+  { title: 'Description', field: 'description' },
 ];
 
 const CategoriesTable = () => {
-  const [data, setData] = useState(defaultData);
+  const [state, setState] = useState({
+    columns,
+    data: [],
+  });
 
   return (
     <MaterialTable
       title="Categories"
-      columns={columns}
-      data={data}
+      columns={state.columns}
+      data={state.data}
       editable={{
         onRowAdd: (newData) => new Promise((resolve) => {
           resolve();
-          setData((prevData) => ([...prevData, newData]));
+          setState((prevState) => {
+            const data = [...prevState.data, newData];
+            return { ...prevState, data };
+          });
         }),
         onRowUpdate: (newData, oldData) => new Promise((resolve) => {
           resolve();
           if (oldData) {
-            setData((prevData) => {
-              const rows = prevData;
-              const index = rows.indexOf(oldData);
-              rows[index] = newData;
-              return rows;
+            setState((prevState) => {
+              const data = [...prevState.data];
+              data[data.indexOf(oldData)] = newData;
+              return { ...prevState, data };
             });
           }
         }),
         onRowDelete: (oldData) => new Promise((resolve) => {
-          setData((prevData) => {
-            const rows = prevData;
-            const index = rows.indexOf(oldData);
-            return rows.splice(index, 1);
-          });
           resolve();
+          setState((prevState) => {
+            const data = [...prevState.data];
+            data.splice(data.indexOf(oldData), 1);
+            return { ...prevState, data };
+          });
         }),
       }}
     />
