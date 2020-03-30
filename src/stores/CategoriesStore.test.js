396a243cb94581f9c9fa5873d00f6dce97387500
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2020-03-30 16:07:45 -0600

    Refactor mobx unit testing

--- src/stores/CategoriesStore.test.js
+++ src/stores/CategoriesStore.test.js
@@ -11,49 +11,59 @@ describe('CategoriesStore', () => {
 		store = new CategoriesStore();
 	});
 
-	it('Should create a new category with addCategory action', () => {
-		const object = { name: 'Food', description: 'Test' };
-		store.addCategory(object);
-		expect(store.categories.length).toBe(1);
-		const [ first ] = store.categories;
-		expect(first.name).toBe(object.name);
-		expect(first.description).toBe(object.description);
+	describe('addCategory action', () => {});
+
+	describe('addCategory action', () => {
+		it('Should create a new category', () => {
+			const object = { name: 'Food', description: 'Test' };
+			store.addCategory(object);
+			expect(store.categories.length).toBe(1);
+			const [ first ] = store.categories;
+			expect(first.name).toBe(object.name);
+			expect(first.description).toBe(object.description);
+		});
 	});
 
-	it('Should delete a specific category with deleteCategory action', () => {
-		const object = { name: 'Food', description: 'Test' };
-		store.addCategory(object);
-		expect(store.categories.length).toBe(1);
-		const [ first ] = store.categories;
-		store.deleteCategory([ first.id ]);
-		expect(store.categories.length).toBe(0);
+	describe('deleteCategory action', () => {
+		it('Should delete a specific category', () => {
+			const object = { name: 'Food', description: 'Test' };
+			store.addCategory(object);
+			expect(store.categories.length).toBe(1);
+			const [ first ] = store.categories;
+			store.deleteCategory([ first.id ]);
+			expect(store.categories.length).toBe(0);
+		});
 	});
 
-	it('Should update name to a specific category with updateCategory action', () => {
-		const object = { name: 'Food', description: 'Test' };
-		const objectUpdated = { name: 'New food', description: 'Test' };
-		store.addCategory(object);
-		const [ first ] = store.categories;
-		store.updateCategory({ [first.id]: objectUpdated });
-		const [ firstUpdated ] = store.categories;
-		expect(firstUpdated.name).toBe(objectUpdated.name);
+	describe('updateCategory action', () => {
+		it('Should update category when just update the category name', () => {
+			const object = { name: 'Food', description: 'Test' };
+			const objectUpdated = { name: 'New food', description: 'Test' };
+			store.addCategory(object);
+			const [ first ] = store.categories;
+			store.updateCategory({ [first.id]: objectUpdated });
+			const [ firstUpdated ] = store.categories;
+			expect(firstUpdated.name).toBe(objectUpdated.name);
+		});
+
+		it('Should update category when just update the description name', () => {
+			const object = { name: 'Food', description: 'Test' };
+			const objectUpdated = { name: 'Food', description: 'New test' };
+			store.addCategory(object);
+			const [ first ] = store.categories;
+			store.updateCategory({ [first.id]: objectUpdated });
+			const [ firstUpdated ] = store.categories;
+			expect(firstUpdated.description).toBe(objectUpdated.description);
+		});
 	});
 
-	it('Should update description to a specific category with updateCategory action', () => {
-		const object = { name: 'Food', description: 'Test' };
-		const objectUpdated = { name: 'Food', description: 'New test' };
-		store.addCategory(object);
-		const [ first ] = store.categories;
-		store.updateCategory({ [first.id]: objectUpdated });
-		const [ firstUpdated ] = store.categories;
-		expect(firstUpdated.description).toBe(objectUpdated.description);
-	});
-
-	it('Should get category name by id param with getNameByCategoryId computed', () => {
-		const object = { name: 'Food', description: 'Test' };
-		store.addCategory(object);
-		const [ first ] = store.categories;
-		const name = store.getNameByCategoryId(first.id);
-		expect(name).toBe(object.name);
+	describe('getNameByCategoryId computed', () => {
+		it('Should get category name by id param', () => {
+			const object = { name: 'Food', description: 'Test' };
+			store.addCategory(object);
+			const [ first ] = store.categories;
+			const name = store.getNameByCategoryId(first.id);
+			expect(name).toBe(object.name);
+		});
 	});
 });
--- src/stores/ProductsStore.test.js
+++ src/stores/ProductsStore.test.js
@@ -11,81 +11,94 @@ describe('ProductsStore', () => {
 		store = new ProductsStore();
 	});
 
-	it('Should create a new product with addProduct action', () => {
-		const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
-		store.addProduct(object);
-		expect(store.products.length).toBe(1);
-		const [ first ] = store.products;
-		expect(first.name).toBe(object.name);
-		expect(first.description).toBe(object.description);
-		expect(first.price).toBe(object.price);
-		expect(first.stock).toBe(object.stock);
-		expect(first.categoryId).toBe(object.categoryId);
+	describe('addProduct action', () => {
+		it('Should create a new product', () => {
+			const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
+			store.addProduct(object);
+			expect(store.products.length).toBe(1);
+			const [ first ] = store.products;
+			expect(first.name).toBe(object.name);
+			expect(first.description).toBe(object.description);
+			expect(first.price).toBe(object.price);
+			expect(first.stock).toBe(object.stock);
+			expect(first.categoryId).toBe(object.categoryId);
+		});
 	});
 
-	it('Should delete a specific product with deleteProduct action', () => {
-		const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
-		store.addProduct(object);
-		expect(store.products.length).toBe(1);
-		const [ first ] = store.products;
-		store.deleteProduct([ first.id ]);
-		expect(store.products.length).toBe(0);
+	describe('deleteProduct action', () => {
+		it('Should delete a specific product', () => {
+			const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
+			store.addProduct(object);
+			expect(store.products.length).toBe(1);
+			const [ first ] = store.products;
+			store.deleteProduct([ first.id ]);
+			expect(store.products.length).toBe(0);
+		});
 	});
 
-	it('Should update name to a specific product with updateProduct action', () => {
-		const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
-		const objectUpdated = { name: 'Beans', description: 'test', price: 0, stock: 0, categoryId: 1 };
-		store.addProduct(object);
-		const [ first ] = store.products;
-		store.updateProduct({ [first.id]: objectUpdated });
-		const [ firstUpdated ] = store.products;
-		expect(firstUpdated.name).toBe(objectUpdated.name);
+	describe('updateProduct action', () => {
+		it('Should update product when just update the product name', () => {
+			const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
+			const objectUpdated = { name: 'Beans', description: 'test', price: 0, stock: 0, categoryId: 1 };
+			store.addProduct(object);
+			const [ first ] = store.products;
+			store.updateProduct({ [first.id]: objectUpdated });
+			const [ firstUpdated ] = store.products;
+			expect(firstUpdated.name).toBe(objectUpdated.name);
+		});
+
+		it('Should update product when just update the product description', () => {
+			const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
+			const objectUpdated = { name: 'Rice', description: 'new test', price: 0, stock: 0, categoryId: 1 };
+			store.addProduct(object);
+			const [ first ] = store.products;
+			store.updateProduct({ [first.id]: objectUpdated });
+			const [ firstUpdated ] = store.products;
+			expect(firstUpdated.description).toBe(objectUpdated.description);
+		});
+
+		it('Should update product when just update the product price', () => {
+			const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
+			const objectUpdated = { name: 'Rice', description: 'test', price: 5432, stock: 0, categoryId: 1 };
+			store.addProduct(object);
+			const [ first ] = store.products;
+			store.updateProduct({ [first.id]: objectUpdated });
+			const [ firstUpdated ] = store.products;
+			expect(firstUpdated.price).toBe(objectUpdated.price);
+		});
+
+		it('Should update product when just update the product stock', () => {
+			const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
+			const objectUpdated = { name: 'Rice', description: 'test', price: 0, stock: 7896, categoryId: 1 };
+			store.addProduct(object);
+			const [ first ] = store.products;
+			store.updateProduct({ [first.id]: objectUpdated });
+			const [ firstUpdated ] = store.products;
+			expect(firstUpdated.stock).toBe(objectUpdated.stock);
+		});
+
+		it('Should update product when just update the product categoryId', () => {
+			const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
+			const objectUpdated = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 2 };
+			store.addProduct(object);
+			const [ first ] = store.products;
+			store.updateProduct({ [first.id]: objectUpdated });
+			const [ firstUpdated ] = store.products;
+			expect(firstUpdated.categoryId).toBe(objectUpdated.categoryId);
+		});
 	});
 
-	it('Should update description to a specific product with updateProduct action', () => {
-		const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
-		const objectUpdated = { name: 'Rice', description: 'new test', price: 0, stock: 0, categoryId: 1 };
-		store.addProduct(object);
-		const [ first ] = store.products;
-		store.updateProduct({ [first.id]: objectUpdated });
-		const [ firstUpdated ] = store.products;
-		expect(firstUpdated.description).toBe(objectUpdated.description);
-	});
+	describe('productsCount computed', () => {
+		it('Should get products count when there are products', () => {
+			const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
+			store.addProduct(object);
+			const count = store.productsCount;
+			expect(count).toBe(1);
+		});
 
-	it('Should update price to a specific product with updateProduct action', () => {
-		const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
-		const objectUpdated = { name: 'Rice', description: 'test', price: 5432, stock: 0, categoryId: 1 };
-		store.addProduct(object);
-		const [ first ] = store.products;
-		store.updateProduct({ [first.id]: objectUpdated });
-		const [ firstUpdated ] = store.products;
-		expect(firstUpdated.price).toBe(objectUpdated.price);
-	});
-
-	it('Should update stock to a specific product with updateProduct action', () => {
-		const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
-		const objectUpdated = { name: 'Rice', description: 'test', price: 0, stock: 7896, categoryId: 1 };
-		store.addProduct(object);
-		const [ first ] = store.products;
-		store.updateProduct({ [first.id]: objectUpdated });
-		const [ firstUpdated ] = store.products;
-		expect(firstUpdated.stock).toBe(objectUpdated.stock);
-	});
-
-	it('Should update categoryId to a specific product with updateProduct action', () => {
-		const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
-		const objectUpdated = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 2 };
-		store.addProduct(object);
-		const [ first ] = store.products;
-		store.updateProduct({ [first.id]: objectUpdated });
-		const [ firstUpdated ] = store.products;
-		expect(firstUpdated.categoryId).toBe(objectUpdated.categoryId);
-	});
-
-	it('Should get products count with productsCount computed', () => {
-		const object = { name: 'Rice', description: 'test', price: 0, stock: 0, categoryId: 1 };
-		store.addProduct(object);
-		const count = store.productsCount;
-		expect(count).toBe(1);
+		it('Should get products count when there are not products', () => {
+			const count = store.productsCount;
+			expect(count).toBe(0);
+		});
 	});
 });
